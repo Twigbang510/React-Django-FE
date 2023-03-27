8994234ee52ac53c4cdc79a6e9d69028e248cba7
Twig <truong.nguyen200207@vnuk.edu.vn>
2023-03-27 16:55:34 +0700

    fix: get

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     category : "",
     joke: ""
   })
+  const [data,setData] = useState([])
   const [flag, setFlag] = useState({
     nsfw: false,
     religious: false,
@@ -41,7 +42,6 @@ function App() {
       [name]: checked
     }));
   };
-  
   const getJoke = () => {
     axios
       .get(`https://v2.jokeapi.dev/joke/${checkedValues}?type=single`)
@@ -62,78 +62,150 @@ function App() {
   };
   function postJoke (event) {
     event.preventDefault()
-    axios.post('http://127.0.0.1:8000/api/joke/', post,flag,jokeId )
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/joke/',
+      data: {
+        category: jokes.category,
+        joke: jokes.joke,
+        flag,
+        joke_id: jokes.id
+      }
+
+    })
     .then(response => console.log(response))
-    .catch(err => console.log(err))
   }
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/joke/')
+      .then(reponse => {
+        setData(reponse.data);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
   return (
     <div>
-      <input
-        value="Programming"
-        type="checkbox"
-        onChange={handleCheckboxChange}
-      />
-      <label>Programming</label>
-      <input value={"Misc"} type="checkbox" onChange={handleCheckboxChange} />
-      <label>Misc</label>
-      <label>
-        NSFW:
-        <input
-          type="checkbox"
-          name="nsfw"
-          checked={flag.nsfw}
-          onChange={handleFlagChange}
-        />
-      </label>
-      <label>
-        Religious:
-        <input
-          type="checkbox"
-          name="religious"
-          checked={flag.religious}
-          onChange={handleFlagChange}
-        />
-      </label>
-      <label>
-        Political:
-        <input
-          type="checkbox"
-          name="political"
-          checked={flag.political}
-          onChange={handleFlagChange}
-        />
-      </label>
-      <label>
-        Racist:
-        <input
-          type="checkbox"
-          name="racist"
-          checked={flag.racist}
-          onChange={handleFlagChange}
-        />
-      </label>
-      <label>
-        Sexist:
-        <input
-          type="checkbox"
-          name="sexist"
-          checked={flag.sexist}
-          onChange={handleFlagChange}
-        />
-      </label>
-      <label>
-        Explicit:
-        <input
-          type="checkbox"
-          name="explicit"
-          checked={flag.explicit}
-          onChange={handleFlagChange}
-        />
-      </label>
+      <div className="category_checkbox">
+        <h1>Category</h1>
+        <label>
+          Programming
+          <input
+            value="Programming"
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+        <label>
+          Misc
+          <input
+            value={"Misc"}
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+        <label>
+          Dark
+          <input
+            value={"Dark"}
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+        <label>
+          Pun
+          <input
+            value={"Pun"}
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+        <label>
+          Spooky
+          <input
+            value={"Spooky"}
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+        <label>
+          Christmas
+          <input
+            value={"Christmas"}
+            type="checkbox"
+            onChange={handleCheckboxChange}
+          />
+        </label>
+      </div>
+      <div className="flag_checkbox">
+        <h1>Flags</h1>
+        <label>
+          NSFW:
+          <input
+            type="checkbox"
+            name="nsfw"
+            checked={flag.nsfw}
+            onChange={handleFlagChange}
+          />
+        </label>
+        <label>
+          Religious:
+          <input
+            type="checkbox"
+            name="religious"
+            checked={flag.religious}
+            onChange={handleFlagChange}
+          />
+        </label>
+        <label>
+          Political:
+          <input
+            type="checkbox"
+            name="political"
+            checked={flag.political}
+            onChange={handleFlagChange}
+          />
+        </label>
+        <label>
+          Racist:
+          <input
+            type="checkbox"
+            name="racist"
+            checked={flag.racist}
+            onChange={handleFlagChange}
+          />
+        </label>
+        <label>
+          Sexist:
+          <input
+            type="checkbox"
+            name="sexist"
+            checked={flag.sexist}
+            onChange={handleFlagChange}
+          />
+        </label>
+        <label>
+          Explicit:
+          <input
+            type="checkbox"
+            name="explicit"
+            checked={flag.explicit}
+            onChange={handleFlagChange}
+          />
+        </label>
+      </div>
 
       <button onClick={getJoke}>Get Joke</button>
 
       <button onClick={postJoke}>Post joke</button>
+
+      <div>
+        {data.map((item) => (
+          <div key={item}>
+            <p>{item.joke}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
